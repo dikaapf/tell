d6dc1617c30916488f95fde4d6e56c97ea6f2382
Priyo Faqih <dikaapf@gmail.com>
2021-12-08 17:54:26 +0700

    update

---
+++ alumni.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TeLL - Trisakti Electronic Learning - Universitas Trisakti</title>
+<style type="text/css">
+<!--
+body {
+	background-color: #CCC;
+	font-family: Verdana, Geneva, sans-serif;
+}
+h1 {
+	font-size: 16px;
+	color: #009;
+}
+h2 {
+	font-size: 14px;
+	color: #009;
+}
+h3 {
+	font-size: 12px;
+	color: #009;
+}
+h4 {
+	font-size: 10px;
+	color: #009;
+}
+body,td,th {
+	font-size: 12px;
+}
+.tell {
+	text-align: justify;
+}
+.tell {
+	text-align: justify;
+}
+-->
+</style>
+<script language="JavaScript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.0
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && document.getElementById) x=document.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+</head>
+
+<body onLoad="MM_preloadImages('2.jpg','alumni1.jpg','alumni2.jpg','alumni3.jpg','alumni4.jpg')">
+<table width="1025" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td colspan="3"><script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<table width="1024" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><img src="images/header-tell2.jpg" width="1024" height="115" alt="TeLL - Trisakti Electronic Learning" /></td>
+  </tr>
+  <tr>
+    <td bgcolor="#EEEEEE">
+      <ul id="Header" class="MenuBarHorizontal">
+        <li><a href="index.php" title="Home">Home</a> </li>
+        <li><a href="tentang_tell.php" title="Tentang Tell">Tentang Tell</a></li>
+        <li><a class="MenuBarItemSubmenu" href="#">Program</a> 
+          <ul>
+            <li><a href="program_magister_manajemen.php">Magister Manajemen</a></li>
+          </ul>
+        </li>
+        <li><a href="fasilitas.php">Fasilitas</a></li>
+        <li><a href="kontak_tell.php">Kontak</a></li>
+      </ul>
+    </td>
+  </tr>
+  <tr>
+    <td><img src="images/header-image1.jpg" width="1024" height="156" alt="TELL - Trisakti Electronic Learning - Universitas Trisakti" /></td>
+  </tr>
+</table>
+<script type="text/javascript">
+<!--
+var MenuBar1 = new Spry.Widget.MenuBar("Header", {imgDown:"SpryAssets/SpryMenuBarDownHover.gif", imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});
+//-->
+</script>
+</td>
+  </tr>
+  <tr>
+    <td width="180" valign="top" bgcolor="#FFFFFF" height="2848"> 
+      <p><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+
+<body onLoad="MM_preloadImages('images/menu-kiri-penerimaanmhsbaru2.jpg','images/menu-kiri-jadwal-ujian2.jpg','images/menu-kiri-2-jurnal.jpg','images/menu-kiri-student-resources2.jpg','images/menu-kiri-alumni2.jpg','images/menu-kiri-jadwal-kuliah2.jpg')">
+<table width="179" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td width="179"><img src="images/menu-kiri-1-judul-agenda.jpg" width="180" height="43" /></td>
+  </tr>
+  <tr>
+    <td><a href="penerimaan_mahasiswa_baru.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Penerimaan Mahasiswa Baru','','images/menu-kiri-penerimaanmhsbaru2.jpg',1)"><img src="images/menu-kiri-penerimaanmhsbaru1.jpg" alt="Penerimaan Mahasiswa Baru" name="Penerimaan Mahasiswa Baru" width="180" height="25" border="0" id="Penerimaan Mahasiswa Baru" /></a></td>
+  </tr>
+  <tr>
+    <td><a href="jadwal_kuliah.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Jadwal Kuliah','','images/menu-kiri-jadwal-kuliah2.jpg',1)"><img src="images/menu-kiri-jadwal-kuliah1.jpg" alt="Jadwal Kuliah" name="Jadwal Kuliah" width="180" height="25" border="0"></a></td>
+  </tr>
+  <tr>
+    <td><a href="jadwal_uts_uas.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Jadwal UTS UAS','','images/menu-kiri-jadwal-ujian2.jpg',1)"><img src="images/menu-kiri-jadwal-ujian1.jpg" alt="Jadwal UTS / UAS" name="Jadwal UTS UAS" width="180" height="25" border="0" id="Jadwal UTS UAS" /></a></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><img src="images/menu-kiri-1-judul-learningresources.jpg" width="180" height="43" alt="Learning Resources" /></td>
+  </tr>
+  <tr>
+    <td><a href="http://www.library.trisakti.ac.id" target="_blank" onMouseOver="MM_swapImage('Jurnal','','images/menu-kiri-2-jurnal.jpg',1)" onMouseOut="MM_swapImgRestore()"><img src="images/menu-kiri-1-jurnal.jpg" alt="Jurnal" name="Jurnal" width="180" height="25" border="0"></a></td>
+  </tr>
+  <tr>
+    <td><a href="student_resources.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Student Resources','','images/menu-kiri-student-resources2.jpg',1)"><img src="images/menu-kiri-student-resources1.jpg" alt="Student Resources" name="Student Resources" width="180" height="25" border="0" id="Student Resources" /></a></td>
+  </tr>
+  <tr>
+    <td><a href="alumni.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Alumni','','images/menu-kiri-alumni2.jpg',1)"><img src="images/menu-kiri-alumni1.jpg" alt="Alumni" name="Alumni" width="180" height="25" border="0" id="Alumni" /></a></td>
+  </tr>
+  <tr>
+    <td><a href="berita.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Berita','','images/menu-kiri-berita1.jpg',1)"><img src="menu-kiri-berita1.jpg" alt="Berita" name="Berita" width="180" height="25" border="0" id="Berita" /></a></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<br />
+    </p>
+    <br /></td>
+    <td width="588" valign="top" bgcolor="#FFFFFF" height="2848"> 
+      <table width="588" border="0" cellspacing="0" cellpadding="15" height="815">
+        <tr> 
+          <td height="57"> 
+            <h1>Alumni</h1>
+          </td>
+        </tr>
+        <tr> 
+          <td valign="top" height="2781"> 
+            <p><a href="#" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image1','','alumni1.jpg',1)"><img name="Image1" border="0" src="alumni1.jpg" width="601" height="741"></a></p>
+            <p><a href="#" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image2','','2.jpg',1)"> 
+              </a><a href="#" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image2','','alumni2.jpg',1)"><img name="Image2" border="0" src="alumni2.jpg" width="598" height="776"></a></p>
+            <p><a href="#" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image3','','alumni3.jpg',1)"><img name="Image3" border="0" src="alumni3.jpg" width="599" height="559"></a></p>
+            <p><a href="#" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('Image4','','alumni4.jpg',1)"><img name="Image4" border="0" src="alumni4.jpg" width="603" height="624"></a></p>
+            <p>&nbsp;</p>
+          </td>
+        </tr>
+      </table>
+    </td>
+    <td width="257" valign="top" bgcolor="#FFFFFF" height="2848"> 
+      <table width="200" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><img src="login-web-ct1.jpg" alt="Login" width="256" height="155" border="0" usemap="#login_web_Tell" /> 
+      <map name="login_web_Tell" id="login_web_Tell"> 
+        <area shape="rect" coords="59,53,227,72" href="http://online.tell.trisakti.ac.id/learning" target="_blank" alt="Student Login" />
+        <area shape="rect" coords="61,84,228,104" href="http://online.tell.trisakti.ac.id/learning" target="_blank" alt="Tutor Login" />
+        <area shape="rect" coords="61,117,227,136" href="http://online.tell.trisakti.ac.id/learning" target="_blank" alt="Admin Login" />
+      </map>
+    </td>
+  </tr>
+  <tr>
+    <td>
+	<p><a href="http://www.trijurnal.lemlit.trisakti.ac.id/index.php/ber"><img src="images/open webmail.jpg" width="118" height="70" alt="Webmail" /><img src="images/FB.jpg" width="70" height="69" alt="TeLL on Facebook" /><img src="images/twitter.jpg" width="64" height="63" alt="Follow TeLL on Twitter" /></td>
+  </tr>
+  <tr>
+    <td> 
+      <p><a href="https://register.tell.trisakti.id"><img src="daftaronline.png" width="124" height="43" border="0"></a></p>
+      <p><a href="http://www.trijurnal.lemlit.trisakti.ac.id/index.php/ber"><img src="review.png" width="124" height="43" border="0"></a></p>
+      <p><a href="http://spada.ristekdikti.go.id/s"><img src="daringdikti.png" width="124" height="43" border="0"></a></p>
+      <p>&nbsp;</p>
+    </td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
+      &nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="3" bgcolor="#FFFFFF">
+      <img src="images/footer.jpg" width="1024" height="50" />    </td>
+  </tr>
+</table>
+</body>
+</html>
